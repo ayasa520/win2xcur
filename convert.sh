9577b5670249a9a53e7d6a67e17ca6b72b3c8bd1
Rikka <1103670381@qq.com>
2022-10-06 01:16:31 +0800

    自动生成不同尺寸 config

--- convert.sh
+++ convert.sh
@@ -1,27 +1,57 @@
 #!/bin/bash
 
 has_command() {
-  "$1" -v $1 > /dev/null 2>&1
+	"$1" -v $1 >/dev/null 2>&1
 }
 
 function create {
 	cd "$SRC"
 	mkdir -p x1 x1_25 x1_5 x2
 
+	tmp_fifofile="/tmp/$$.fifo"
+	mkfifo $tmp_fifofile  # 新建一个FIFO类型的文件
+	exec 6<>$tmp_fifofile # 将FD6指向FIFO类型
+	rm $tmp_fifofile      #删也可以，
+
+	thread_num=5 # max thread count
+	for ((i = 0; i < ${thread_num}; i++)); do
+		echo
+	done >&6
+
 	cd "$SRC"/$1
-	find . -name "*.png" -type f -exec sh -c 'inkscape -o "../x1/${0%}" -w 32 -h 32 $0' {} \;
-	find . -name "*.png" -type f -exec sh -c 'inkscape -o "../x1_25/${0%}" -w 40 -w 40 $0' {} \;
-	find . -name "*.png" -type f -exec sh -c 'inkscape -o "../x1_5/${0%}" -w 48 -w 48 $0' {} \;
-	find . -name "*.png" -type f -exec sh -c 'inkscape -o "../x2/${0%}" -w 64 -w 64 $0' {} \;
+
+	cat /dev/null >inkscape.log
+
+	pngs=$(ls *.png)
+
+    mark=''
+    count=0 
+    all=$(ls -l *.png| wc -l)
+    all=$((all*4))
+	for factor in "${!SIZE_ARRAY[@]}"; do
+		for png_file in ${pngs}; do
+			read -u6
+			{
+				inkscape -o "../$factor/$png_file" -w "${SIZE_ARRAY[$factor]}" -h "${SIZE_ARRAY[$factor]}" $png_file 2>inkscape.log
+				echo >&6
+			} &
+            count=$((count+1))
+            printf "progress: %s\r" "$((count*100/all))%"
+		done
+	done 
+    wait
+    echo 
 
 	cd "$SRC"
 
 	OUTPUT="$BUILD"/cursors
 	ALIASES="$SRC"/cursorList
-
-	if [ ! -d "$BUILD" ]; then
-		mkdir "$BUILD"
-	fi
+    if [ -d "$BUILD" ]; then
+        rm -rf "$BUILD"
+    fi
+    
+    mkdir "$BUILD"
+        
 	if [ ! -d "$OUTPUT" ]; then
 		mkdir "$OUTPUT"
 	fi
@@ -31,7 +61,7 @@ function create {
 		BASENAME="$CUR"
 		BASENAME="${BASENAME##*/}"
 		BASENAME="${BASENAME%.*}"
-		
+
 		xcursorgen "$CUR" "$OUTPUT/$BASENAME"
 	done
 	echo -e "Generating cursor theme... DONE"
@@ -48,7 +78,7 @@ function create {
 			continue
 		fi
 		ln -sr "$FROM" "$TO"
-	done < "$ALIASES"
+	done <"$ALIASES"
 	echo -e "Generating shortcuts... DONE"
 
 	cd "$PWD"
@@ -57,173 +87,121 @@ function create {
 	INDEX="$OUTPUT/../index.theme"
 	if [ ! -e "$OUTPUT/../$INDEX" ]; then
 		touch "$INDEX"
-		echo -e "[Icon Theme]\nName=$THEME\n" > "$INDEX"
+		echo -e "[Icon Theme]\nName=$THEME\n" >"$INDEX"
 	fi
 	echo -e "Generating Theme Index... DONE"
 }
 
-read -p "是否使用已有的配置文件? 如果是, 将已有配置文件放入 config 目录 [y/n]" input
+function process_config {
+	sed -i '/^#/d' "$1"
+	content=$(cat "$1")
+	cat /dev/null >"$1"
 
-if [ ! "$(which xcursorgen 2> /dev/null)" ]; then
-  echo xorg-xcursorgen needs to be installed to generate the cursors.
-  if has_command zypper; then
-    sudo zypper in xorg-xcursorgen
-  elif has_command apt; then
-    sudo apt install xorg-xcursorgen
-  elif has_command dnf; then
-    sudo dnf install -y xorg-xcursorgen
-  elif has_command dnf; then
-    sudo dnf install xorg-xcursorgen
-  elif has_command pacman; then
-    sudo pacman -S --noconfirm xorg-xcursorgen
-  fi
+	for factor in "${!SIZE_ARRAY[@]}"; do
+		echo "$content" | awk -v factor=$factor -v size=${SIZE_ARRAY[$factor]} '{match($4, "/([^/]+)\\.png$")} {$4=factor""substr($4, RSTART,RLENGTH)} {$2=$2*size/$1;$3=$3*size/$1;$1=size}{printf "%d\t%.0f\t%.0f\t%s\t%d\n",$1,$2,$3,$4,$5}' >>$1
+	done
+
+}
+
+if [ ! "$(which xcursorgen 2>/dev/null)" ]; then
+	echo xorg-xcursorgen needs to be installed to generate the cursors.
+	if has_command zypper; then
+		sudo zypper in xorg-xcursorgen
+	elif has_command apt; then
+		sudo apt install -y x11-apps
+	elif has_command dnf; then
+		sudo dnf install -y xorg-xcursorgen
+	elif has_command dnf; then
+		sudo dnf install xorg-xcursorgen
+	elif has_command pacman; then
+		sudo pacman -S --noconfirm xorg-xcursorgen
+	fi
 fi
 
-if [ ! "$(which inkscape 2> /dev/null)" ]; then
-  echo inkscape needs to be installed to generate the cursors.
-  if has_command zypper; then
-    sudo zypper in inkscape
-  elif has_command apt; then
-    sudo apt install inkscape
-  elif has_command dnf; then
-    sudo dnf install -y inkscape
-  elif has_command dnf; then
-    sudo dnf install inkscape
-  elif has_command pacman; then
-    sudo pacman -S --noconfirm inkscape
-  fi
+if [ ! "$(which inkscape 2>/dev/null)" ]; then
+	echo inkscape needs to be installed to generate the cursors.
+	if has_command zypper; then
+		sudo zypper in inkscape
+	elif has_command apt; then
+		sudo apt install -y inkscape
+	elif has_command dnf; then
+		sudo dnf install -y inkscape
+	elif has_command dnf; then
+		sudo dnf install inkscape
+	elif has_command pacman; then
+		sudo pacman -S --noconfirm inkscape
+	fi
 fi
 
-if [ ! "$(which win2xcur 2> /dev/null)" ]; then
-  echo xcursorgen needs to be installed to generate the cursors.
-  pip install win2xcur
+if [ ! "$(which win2xcur 2>/dev/null)" ]; then
+	echo win2xcur needs to be installed to generate the cursors.
+	pip install win2xcur
 fi
 
 SRC=$PWD/src
 BUILD="$SRC/../dist"
-
+declare -A SIZE_ARRAY
+SIZE_ARRAY=([x1]=24 [x1_25]=30 [x1_5]=36 [x2]=48)
 mkdir -p "$SRC/output" "$SRC/config"
 
 cd "$SRC/wincursors"
 
-INSTALL_INF=$(find -name "*.inf")
+INSTALL_INF=$(ls *.inf)
+if [[ $(file -i $INSTALL_INF) =~ "iso-8859-1" ]]; then 
+    iconv -f gb18030 -t UTF-8 $INSTALL_INF -o $INSTALL_INF
+fi 
+
 declare -A CURS
 
-CURS=([help]="help" [work]="progress" [busy]="wait" [cross]="crosshair" [text]="text" [handwrt]="pencil" [unavailable]="circle" [vert]="size_ver" [horz]="size_hor" [dgn1]="size_fdiag" [dgn2]="size_bdiag" [move]="fleur" [pointer]="default" [link]="pointer")
+CURS=([help]="help" [work]="progress" [busy]="wait" [cross]="crosshair" [text]="text" [handwrt]="pencil" [unavailable]="circle" [vert]="size_ver" [horz]="size_hor" [dgn1]="size_fdiag" [dgn2]="size_bdiag" [move]="fleur" [pointer]="default" [link]="pointer" [hand]="pencil")
 
-THEME=$(grep 'SCHEME_NAME' *.inf | tail -n 1 | cut -f2 -d"=" | sed -e's/"//g' -e 's/^\s*//g'   -e 's/\s*$//g')" Cursors"
-sed -i "s/\r//g" *.inf
+THEME=$(grep 'SCHEME_NAME' *.inf | tail -n 1 | cut -f2 -d"=" | sed -e's/"//g' -e 's/^\s*//g' -e 's/\s*$//g')" Cursors"
+# sed -i "s/\r//g" *.inf
 
-for key in "${!CURS[@]}"
-do
-    name=$(grep "^${key}" *.inf | tail -n 1 | cut -f2 -d'=' | sed -e "s/\s*\([A-Za-z0-9 ]*\)\.ani/\1.ani/" -e 's/\"//g' -e 's/^\s*//g'   -e 's/\s*$//g')
-    echo "rename $name to ${CURS[${key}]}.ani"
-    perl-rename "s/$name/${CURS[${key}]}.ani/" *.ani
+for key in "${!CURS[@]}"; do
+	name=$(grep -i "^${key}" *.inf | tail -n 1 | cut -f2 -d'=' | sed -e "s/\s*\([A-Za-z0-9 ]*\)\.ani/\1.ani/" -e 's/\"//g' -e 's/^\s*//g' -e 's/\s*$//g')
+	echo "rename $name to ${CURS[${key}]}.ani"
+	perl-rename "s/$name/${CURS[${key}]}.ani/" *.ani
 done
 
 ANIS=$(ls *.ani)
 
 # for ani in ${ANIS[@]}
-# do 
+# do
 #     # echo $SRC/wincusors/${ani}
 #     # ../../ani2ico $SRC/wincusors/${ani}
 #     # 暂时不研究, ani 看不懂. 这个直接转换过的尺寸只有 160px 一种, 所以只是借用来提取 png
 # done
 
 # 如果不自己写 config, 这步完了直接用就行了
-    # win2xcur ./*.{ani,cur} -o ../output/ 
-    win2xcur ./*.ani -o ../output/
+# win2xcur ./*.{ani,cur} -o ../output/
+win2xcur ./*.ani -o ../output/
 
-case $input in
-        [yY]*)
-                cd "$SRC/output"
-                if [ -d "$SRC/png" ]; then
-                    rm -rf "$SRC/png"
-                fi
-                mkdir -p "$SRC/png"
+cd "$SRC/output"
+if [ -d "$SRC/png" ]; then
+	rm -rf "$SRC/png"
+fi
+mkdir -p "$SRC/png"
 
-                XCURS=$(ls)
+XCURS=$(ls)
 
-                for xcur in ${XCURS}
-                do
-                    xcur2png -d ../png ${xcur}
-                done
-
-                # config 目前得自己写, 也许 ani 文件中有信息, 但我暂时看不懂
-                rm *.conf 
-                # generate pixmaps from svg source
-
-                create png
+for xcur in ${XCURS}; do
+	"$SRC"/xcur2png -d ../png ${xcur}
+done
 
 
-                cd "$SRC"
-                rm -rf x1 x1_25 x1_5 x2 output
-                ;;
-        [nN]*)
-                OUTPUT="$BUILD"/cursors
-                if [ -d "$BUILD" ]; then
-                    rm -rf "$BUILD"
-                fi
-                mkdir -p "$OUTPUT"
-                mv "$SRC"/output/* "$OUTPUT"
+perl-rename 's/(.*).conf/$1.cursor/' *.conf
+rm ../config/*
+mv *.cursor ../config
+# generate pixmaps from svg source
+cd ../config
+CONFS=$(ls *.cursor)
 
-                INDEX="$OUTPUT/../index.theme"
-                if [ ! -e "$OUTPUT/../$INDEX" ]; then
-                    touch "$INDEX"
-                    echo -e "[Icon Theme]\nName=$THEME\n" > "$INDEX"
-                fi
-                cd $OUTPUT
-                ln -s progress 00000000000000020006000e7e9ffc3f 
-                ln -s size_ver 00008160000006810000408080010102 
-                ln -s circle 03b6e0fcb3499374a867c041f52298f0 
-                ln -s progress 08e8e1c95fe2fc01f976f1e063a24ccd 
-                ln -s progress 3ecb610c1bf2410f44200f48c40d3599 
-                ln -s help 5c6cd98b3f3ebcb1f9c7f1c204630408 
-                ln -s pointer 9d800788f1b08800ae810202380a0822 
-                ln -s help d9ce0ab605698f320427677b458ad60b 
-                ln -s pointer e29285e634086352946a0e7090d73106 
-                ln -s fleur all-scroll
-                ln -s default arrow 
-                ln -s size_bdiag bottom_left_corner 
-                ln -s size_fdiag bottom_right_corner 
-                ln -s size_ver bottom_side 
-                ln -s size_hor col-resize
-                ln -s crosshair cross 
-                ln -s size_hor e-resize 
-                ln -s progress half-busy
-                ln -s size_hor h_double_arrow 
-                ln -s default left_ptr 
-                ln -s progress left_ptr_watch 
-                ln -s size_hor left_side 
-                ln -s bottom_left_corner ll_angle 
-                ln -s bottom_right_corner lr_angle 
-                ln -s top_right_corner ne-resize
-                ln -s size_ver n-resize
-                ln -s top_left_corner nw-resize 
-                ln -s help question_arrow 
-                ln -s size_hor right_side 
-                ln -s size_ver row-resize 
-                ln -s size_hor sb_h_double_arrow 
-                ln -s size_ver sb_v_double_arrow 
-                ln -s bottom_right_corner se-resize 
-                ln -s fleur size_all 
-                ln -s col-resize split_h 
-                ln -s row-resize split_v 
-                ln -s size_ver s-resize 
-                ln -s bottom_left_corner sw-resize
-                ln -s size_fdiag top_left_corner 
-                ln -s size_bdiag top_right_corner 
-                ln -s size_ver top_side 
-                ln -s top_left_corner ul_angle 
-                ln -s top_right_corner ur_angle 
-                ln -s wait watch 
-                ln -s help whats_this 
-                ln -s size_hor w-resize
+for CONF in ${CONFS}; do
+	process_config ${CONF}
+done
 
-                ;;
-        *)
-                echo "Just enter y or n, please."
-                exit
-                ;;
-esac
+create png
 
+cd "${SRC}"
+rm -rf x1 x1_25 x1_5 x2 output
